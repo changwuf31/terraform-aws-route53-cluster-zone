48bb097a5dce193cd68c5b65ef255c7411ef7b17
airpaz <airpaz@thor>
2019-03-10 00:22:14 +0700

    initial changes: remove parent zone

--- main.tf
+++ main.tf
@@ -11,54 +11,16 @@ module "label" {
 
 data "aws_region" "default" {}
 
-data "template_file" "zone_name" {
-  count    = "${var.enabled == "true" ? 1 : 0}"
-  template = "${replace(var.zone_name, "$$$$", "$")}"
-
-  vars {
-    namespace        = "${var.namespace}"
-    name             = "${var.name}"
-    stage            = "${var.stage}"
-    id               = "${module.label.id}"
-    attributes       = "${module.label.attributes}"
-    parent_zone_name = "${join("", null_resource.parent.*.triggers.zone_name)}"
-    region           = "${data.aws_region.default.name}"
-  }
-}
-
-resource "null_resource" "parent" {
-  count = "${var.enabled == "true" ? 1 : 0}"
-
-  triggers = {
-    zone_id   = "${format("%v", length(var.parent_zone_id) > 0 ? join(" ", data.aws_route53_zone.parent_by_zone_id.*.zone_id) : join(" ", data.aws_route53_zone.parent_by_zone_name.*.zone_id) )}"
-    zone_name = "${format("%v", length(var.parent_zone_id) > 0 ? join(" ", data.aws_route53_zone.parent_by_zone_id.*.name) : join(" ", data.aws_route53_zone.parent_by_zone_name.*.name) )}"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-data "aws_route53_zone" "parent_by_zone_id" {
-  count   = "${var.enabled == "true" ? signum(length(var.parent_zone_id)) : 0}"
-  zone_id = "${var.parent_zone_id}"
-}
-
-data "aws_route53_zone" "parent_by_zone_name" {
-  count = "${var.enabled == "true" ? signum(length(var.parent_zone_name)) : 0}"
-  name  = "${var.parent_zone_name}"
-}
-
 resource "aws_route53_zone" "default" {
   count = "${var.enabled == "true" ? 1 : 0}"
-  name  = "${join("", data.template_file.zone_name.*.rendered)}"
+  name  = "${var.zone_name}"
   tags  = "${module.label.tags}"
 }
 
 resource "aws_route53_record" "ns" {
   count   = "${var.enabled == "true" ? 1 : 0}"
-  zone_id = "${join("", null_resource.parent.*.triggers.zone_id)}"
-  name    = "${join("", aws_route53_zone.default.*.name)}"
+  zone_id = "${aws_route53_zone.default.*.id}"
+  name    = "${var.zone_name}"
   type    = "NS"
   ttl     = "60"
 
