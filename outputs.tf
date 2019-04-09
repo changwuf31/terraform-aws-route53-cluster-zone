263128639210053517e759315bc64b2becae8ad4
airpaz <airpaz@thor>
2019-04-09 23:41:46 +0700

    add zone_name_servers output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,10 @@ output "zone_id" {
   description = "Route53 DNS Zone ID"
 }
 
+output "zone_name_servers" {
+  value       = "${aws_route53_zone.default.name_servers}"
+}
+
 output "zone_name" {
   value       = "${replace(join("", aws_route53_zone.default.*.name), "/\\.$$/", "")}"
   description = "Route53 DNS Zone name"
